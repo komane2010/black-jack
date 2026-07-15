6cc9935b1e3d8317a6b1b3796dc8c5c7b0553acd
komane2010 <komaneadimi@gmail.com>
2026-07-15 15:33:14 +0100

    chang1

--- blqckgake.py
+++ blqckgake.py
@@ -18,16 +18,33 @@ while True:
     pc.append(choice())
     print (f"your cards {user} score={lestsum(user)}")
     print (f"pc cards {pc[0]}")
-    
-    if lestsum(user)==21:
+    while True:
+      if lestsum(user)==21:
+        print("you win")
+        break
+      elif lestsum(user)>21:
+        print(f"your scor is {lestsum(user)}")
+        print ("you lost")
+        break
+      else:
+        get=input("do you want another card (y or n)")
+        if get=="y":
+          user.append(choice())
+          print (f"your scor {lestsum(user)}")
+        else:
+          break
+    if lestsum(pc)<17:
+      pc.append(choice())
+    if  lestsum(pc)>21:
+      print (f"the scor of pc {lestsum(pc)}")
       print("you win")
-      break
-    elif lestsum(user)>21:
-      print ("you lost")
-      break
-    get=input("do you want another card (y or n)")
-    if get=="y":
-      user.append(choice())
+    elif lestsum(pc)>lestsum(user):
+      print (f"the scor of pc {lestsum(pc)}")
+      print("you lost")
+    else:
+      print("you win")
+      print (f"the scor of pc {lestsum(pc)}")
+      print (f"the scor of user {lestsum(user)}")
   elif a =="n":
     break
   else:
